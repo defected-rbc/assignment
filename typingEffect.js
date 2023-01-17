ae08ec70a38c4a4fd5a4f21ca9a59d169f7b1f20
agggunjan <122469486+agggunjan@users.noreply.github.com>
2023-01-17 15:46:53 +0530

    Add files via upload

---
+++ typingEffect.js
@@ -0,0 +1,37 @@
+const words = ["NOTES", "ASSIGNMENT", "PROJECTS", "TUTORIAL SHEET", "KNOW MORE...", ":)))"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function () {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 300);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function () {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
